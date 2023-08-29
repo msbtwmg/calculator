d62d119ec8e58d0ba758b8489cf68d14272d1f8b
Maisha <maishaboto@gmail.com>
2023-08-29 13:57:52 +0200

    Updated button functions, clearer function names

--- main.js
+++ main.js
@@ -1,65 +1,96 @@
-var firstNumber, secondNumber, operator, displayValue;
-
+var firstNumber = "";
+var secondNumber = "";
+var operator = "";
+var displayValue = "0";
+var display = document.querySelector(".display");
 
 function add(a, b) {
-    return a + b; 
+    return a + b;
 }
 
 function subtract(a, b) {
-    return a - b; 
+    return a - b;
 }
 
 function multiply(a, b) {
-    return a * b; 
+    return a * b;
 }
 
 function divide(a, b) {
-    return a / b; 
+    return a / b;
 }
 
-
-function operate(firstNumber, operator, secondNumber) {
-    switch (operator) {
+function operate(a, op, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    switch (op) {
         case "+":
-            add(firstNumber, secondNumber);
-            break;
+            return add(a, b);
         case "-":
-            subtract(firstNumber, secondNumber);
+            return subtract(a, b);
         case "x":
-            multiply(firstNumber, secondNumber);
-            break;
+            return multiply(a, b);
         case "/":
-            divide(firstNumber, secondNumber);
+            return divide(a, b);
         default:
-            break;
+            return NaN; // Handle invalid operations
+    }
+    operator = "";
+}
+
+function updateDisplay() {
+    display.textContent = displayValue;
+}
+
+function clearCalculator() {
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
+    displayValue = "";
+    updateDisplay();
+}
+
+function numberClick(number) {
+    if (displayValue === "0" || operator !== "") {
+        displayValue = number;
+    } else {
+        displayValue += number;
+    }
+    updateDisplay();
+}
+
+function operatorClick(op) {
+    if (operator !== "") {
+        secondNumber = displayValue;
+        firstNumber = operate(firstNumber, operator, secondNumber);
+        displayValue = firstNumber;
+    } else {
+        firstNumber = displayValue;
+    }
+    operator = op;
+    updateDisplay();
+}
+
+function equalClick() {
+    if (operator !== "") {
+        secondNumber = displayValue;
+        firstNumber = operate(firstNumber, operator, secondNumber);
+        displayValue = firstNumber;
+        operator = "";
+        updateDisplay();
     }
 }
-function getButtonText(button) {
-    var buttonText = button.textContent;
-    return buttonText;
-}
 
-function displayNumButton(e) {
-    if (e.target.classList.contains("number")) {
-        var display = document.querySelector(".display");
-        displayValue = display.innerHTML += getButtonText(e.target);
+document.addEventListener("click", function (e) {
+    const target = e.target;
+
+    if (target.classList.contains("number")) {
+        numberClick(target.textContent);
+    } else if (target.classList.contains("operator")) {
+        operatorClick(target.textContent);
+    } else if (target.classList.contains("equal")) {
+        equalClick();
+    } else if (target.classList.contains("clear")) {
+        clearCalculator();
     }
-
-}
-
-
-function operateButton(e) {
-    if (e.target.classList.contains("operator")) { 
-        firstNumber = displayValue;    
-        var display = document.querySelector(".display");
-        display.textContent = "";
-       
-        
-    }
-}
-
-document.addEventListener("click", displayNumButton);
-document.addEventListener("click", operateButton);
-
-
-console.log(firstNumber);
+});
